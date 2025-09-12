717a9c446208cbbed2af608b3ff6d95ec294a416
iarsh <iarshkaushal@gmail.com>
2025-09-12 18:42:41 +0530

    Polymorphism introduction and compile time and run time polymorphism example files are created

---
+++ src/main/java/org/akk/oop_basic/f_polymorphism/RTPolymorphismExample.java
@@ -0,0 +1,39 @@
+package org.akk.oop_basic.f_polymorphism;
+
+
+/** Run Time Polymorphism
+ * using method overriding
+ * */
+
+class Animal {
+    void sound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class RTPolymorphismExample {
+    public static void main(String[] args) {
+        Animal a1 = new Dog(); // upcasting
+        Animal a2 = new Cat();
+
+        a1.sound(); // Dog barks (runtime decision)
+        a2.sound(); // Cat meows (runtime decision)
+    }
+}
+
+
+
