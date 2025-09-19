c0255586f093d4bb65a831b2358f6540955df61b
iarsh <iarshkaushal@gmail.com>
2025-09-19 18:10:42 +0530

    Anonymous object examples are created

---
+++ src/main/java/org/akk/oop_advance/anonymous_objects/AnonymousObjectEx1.java
@@ -0,0 +1,30 @@
+package org.akk.oop_advance.anonymous_objects;
+
+
+class UserService {
+    public void registerUser(UserDTO user) {
+        System.out.println("Registering user: " + user.getName());
+    }
+}
+
+class UserDTO {
+    private String name;
+    private String email;
+
+    public UserDTO(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+}
+
+
+
+
+public class AnonymousObjectEx1 {
+    public static void main(String[] args) {
+        new UserService().registerUser(new UserDTO("Ashwani Kumar", "ashwani@example.com"));
+    }
+}
