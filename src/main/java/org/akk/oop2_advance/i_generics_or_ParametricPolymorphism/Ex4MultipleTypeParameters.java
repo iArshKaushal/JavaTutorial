86627640e55e3b329964f27e2517c468f902dd21
iarsh <iarshkaushal@gmail.com>
2025-10-09 18:15:07 +0530

    Examples for Multiple type parameters created

---
+++ src/main/java/org/akk/oop2_advance/i_generics_or_ParametricPolymorphism/Ex4MultipleTypeParameters.java
@@ -0,0 +1,35 @@
+package org.akk.oop2_advance.i_generics_or_ParametricPolymorphism;
+
+
+class Pair<K, V>{
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
+
+public class Ex4MultipleTypeParameters {
+    public static void main(String[] args) {
+
+        ///  Pair<String, Integer>
+        Pair<String, Integer> pair = new Pair<String, Integer>("Age", 35);
+        System.out.println(pair.getKey() + " : " + pair.getValue());
+
+        /// Pair<Integer, String>
+        Pair<Integer, String> pair2 = new Pair<Integer, String>(101, "Ashwani");
+        System.out.println(pair2.getKey() + " : "  + pair2.getValue());
+
+    }
+}
